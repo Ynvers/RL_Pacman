d2ac688153fd327d45292ea3d2b343f3e2550185
ADOHO Nolhan-Nathan Luzolo <luzolo.adoho@gmail.com>
2025-04-15 17:53:10 +0000

    Début du travail

---
+++ game/player.py
@@ -0,0 +1,29 @@
+#Creation of the class player
+
+class Player:
+    def __init__(self, position, speed, sprite, score, history):
+        self.position = position
+        self.speed = speed
+        self.sprite = sprite
+        self.score = score
+        self.history = history
+
+    def move(direction):
+        """
+        A foction to update the postition of the pacman after a move
+        """
+        pass
+
+    def check_collision():
+        """
+        Check if a collision was be
+        """
+        pass
+
+    def eat_pacdots():
+        """
+        Eat the pacdots and update the board after that
+        """
+        pass
+
+    
---
+++ main.py
@@ -0,0 +1,7 @@
+import game.board as board
+import game.player as player
+import game.ghost as ghost
+import pygame
+
+#Pygame setup
+pygame.init()
